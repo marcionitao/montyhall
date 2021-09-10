962c93cec2885ce742068f255399b779eeccd919
Marcio Nitao <marcionitao@gmail.com>
2021-09-10 11:53:42 +0200

    finishing Form and Game

---
+++ src/components/NumberInput.tsx
@@ -0,0 +1,29 @@
+import styles from "../styles/NumberInput.module.css"
+
+interface EntradaNumericaProps {
+  text: string
+  value: number
+  onChange: (newValue: number) => void
+}
+
+export default function numberInput(props: EntradaNumericaProps) {
+
+  const dec = () => {
+    props.onChange(props.value - 1)
+  }
+
+  const inc = () => {
+    props.onChange(props.value + 1)
+  }
+
+  return(
+    <div className={ styles.entradaNumerica }>
+      <span className={ styles.text }>{ props.text }</span>
+      <span className={ styles.value }>{ props.value }</span>
+      <div className={ styles.botoes }>
+        <button className={ styles.btn } onClick={ dec }> - </button>
+        <button className={ styles.btn } onClick={ inc }> + </button>
+      </div>
+    </div>
+  )
+} 
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,8 +1,13 @@
 import Card from '../components/Card';
 import styles from '../styles/Form.module.css'
 import Link from "next/link"
+import NumberInput from '../components/NumberInput';
+import { useState } from 'react';
 
 export default function Form() {
+
+  const [qtdePortas, setQtdePortas] = useState(3)
+  const [comPresente, setComPresente] = useState(1)
 
   return (
     <div className={ styles.formulario }>
@@ -10,13 +15,21 @@ export default function Form() {
         <Card bgcolor="#c0392c">
           <h1>Monty Hall</h1>
         </Card>
-        <Card></Card>
+        <Card>
+          <NumberInput text= "number of doors?" 
+            value={ qtdePortas } onChange={ novaQtde => setQtdePortas(novaQtde) }
+          />
+        </Card>
       </div>
       <div>
-        <Card></Card>
+        <Card>
+          <NumberInput text= "door with Gif?" 
+            value={ comPresente } onChange={ novaPortaComPresente => setComPresente(novaPortaComPresente) }
+          />
+        </Card>
         
         <Card bgcolor="#28a085">
-          <Link href={ `/game/4/2` }>
+          <Link href={ `/game/${ qtdePortas }/${ comPresente }` }>
             <h2 className={ styles.link }>Start</h2>
           </Link>
         </Card>
